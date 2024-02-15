aff27e3e574923a7868ef11e7e64440a599ebf0a
TiranMikayelyan <tiranmiq10@gmail.com>
2024-02-15 17:52:52 +0400

    v2

--- cesar.py
+++ cesar.py
@@ -1,7 +1,7 @@
 
-engalphabet = 'abcdefghijklmnopqrstuvwxyz'
-armalphabet = 'աբգդեզէըթժիլխծկհձղճմյնշոչպջռսվտրցւփքևօֆ'
-rusalphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+engalphabet = 'abcdefghijklmnopqrstuvwxyzabc'
+armalphabet = 'աբգդեզէըթժիլխծկհձղճմյնշոչպջռսվտրցւփքևօֆաբգ'
+rusalphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяабв'
 
 text = input("Enter a text: ").lower()
 key = int(input("Enter a key (For English 1-25, For Armenian 1-35, For Russian 1-32): "))
@@ -11,9 +11,9 @@ for i in text:
     if i in engalphabet:
         encrypted_text += engalphabet[(engalphabet.index(i) + key)]
     elif i in armalphabet:
-        encrypted_text += armalphabet[(armalphabet.index(i) + key) ]
+        encrypted_text += armalphabet[(armalphabet.index(i) + key)]
     elif i in rusalphabet:
-        encrypted_text += rusalphabet[(rusalphabet.index(i) + key) ]
+        encrypted_text += rusalphabet[(rusalphabet.index(i) + key)]
     elif i == ' ':
         encrypted_text += ' '  
 
