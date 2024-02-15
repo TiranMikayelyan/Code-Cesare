87e930c83b9958c5fbac0b7a732cbcca89cca67d
TiranMikayelyan <tiranmiq10@gmail.com>
2024-02-15 17:34:52 +0400

    v1

---
+++ cesar.py
@@ -0,0 +1,20 @@
+
+engalphabet = 'abcdefghijklmnopqrstuvwxyz'
+armalphabet = 'աբգդեզէըթժիլխծկհձղճմյնշոչպջռսվտրցւփքևօֆ'
+rusalphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+
+text = input("Enter a text: ").lower()
+key = int(input("Enter a key (For English 1-25, For Armenian 1-35, For Russian 1-32): "))
+encrypted_text = ''
+
+for i in text:
+    if i in engalphabet:
+        encrypted_text += engalphabet[(engalphabet.index(i) + key)]
+    elif i in armalphabet:
+        encrypted_text += armalphabet[(armalphabet.index(i) + key) ]
+    elif i in rusalphabet:
+        encrypted_text += rusalphabet[(rusalphabet.index(i) + key) ]
+    elif i == ' ':
+        encrypted_text += ' '  
+
+print(encrypted_text)
